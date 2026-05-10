57e2db48a3d5d4ddbba4e296728e2a21a75e688c
Mary234870 <maryannmarino691@gmail.com>
2026-05-10 09:16:57 +0800

    Add @Repository annotation to CategoryRepository

--- EcommerceApi/src/main/java/com/ws101/repository/CategoryRepository.java
+++ EcommerceApi/src/main/java/com/ws101/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.ws101.marino.bantillo.EcommerceApi.repository;
 
 import com.ws101.marino.bantillo.EcommerceApi.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
 }
+
