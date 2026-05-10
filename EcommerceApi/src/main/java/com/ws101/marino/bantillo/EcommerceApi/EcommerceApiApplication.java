831b0a3903b8dc8d812773c02d9c23c92d5fcc9e
Mary234870 <maryannmarino691@gmail.com>
2026-05-10 09:46:04 +0800

    Update EcommerceApiApplication.java

--- EcommerceApi/src/main/java/com/ws101/marino/bantillo/EcommerceApi/EcommerceApiApplication.java
+++ EcommerceApi/src/main/java/com/ws101/marino/bantillo/EcommerceApi/EcommerceApiApplication.java
@@ -1,12 +1,16 @@
 package com.ws101.marino.bantillo.EcommerceApi;
 
+import com.ws101.marino.bantillo.EcommerceApi.model.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "com.ws101.marino.bantillo.EcommerceApi")
-public class EcommerceApiApplication {
+@SpringBootApplication
+public class EcommerceapiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApiApplication.class, args);
+        SpringApplication.run(EcommerceapiApplication.class, args);
     }
+
 }
