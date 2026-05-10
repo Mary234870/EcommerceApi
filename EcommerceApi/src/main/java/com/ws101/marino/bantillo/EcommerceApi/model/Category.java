e82930281a1e84dc067969a6de4d3a5b7cce65b7
Mary234870 <maryannmarino691@gmail.com>
2026-05-10 09:06:35 +0800

    Refactor Category class and add products relationship
    Refactor Category class to use Lombok annotations for getters and setters, and add a one-to-many relationship with Product.

--- EcommerceApi/src/main/java/com/ws101/marino/bantillo/EcommerceApi/model/Category.java
+++ EcommerceApi/src/main/java/com/ws101/marino/bantillo/EcommerceApi/model/Category.java
@@ -1,8 +1,22 @@
+package com.ws101.marino.bantillo.EcommerceApi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "categories")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Category {
 
     @Id
@@ -10,4 +24,13 @@ public class Category {
     private Long id;
 
     private String name;
-}
+
+    // LAZY + @JsonIgnore SO NO ERROR WHEN ADDING
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Product> products = new ArrayList<>();
+
+    public Category(String name) {
+        this.name = name;
+    }
+    }
