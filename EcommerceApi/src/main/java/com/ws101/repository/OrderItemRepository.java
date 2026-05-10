58dd3e5ba8d13cc335f74085dea2c8c7d2d7bdf7
Mary234870 <maryannmarino691@gmail.com>
2026-05-10 09:22:00 +0800

    Fix import statement for OrderItem model

--- EcommerceApi/src/main/java/com/ws101/repository/OrderItemRepository.java
+++ EcommerceApi/src/main/java/com/ws101/repository/OrderItemRepository.java
@@ -1,6 +1,6 @@
 package com.ws101.marino.bantillo.EcommerceApi.repository;
 
-import com.ws181.gepollo_rapsing.ecommerceapi.model.OrderItem;
+import com.ws101.marino.bantillo.EcommerceApi.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
